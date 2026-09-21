411b9dca2d1a221e08af80dc07e2ec83e11eb384
Никита Лобосов <lobosovnikita0@gmail.com>
2026-09-21 22:37:02 +0300

    add PATCH hotel and PUT hotel

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import uvicorn
 app = FastAPI()
 
 hotels = [
-    {"id": 1, "title": "Sochi"},
-    {"id": 2, "title": "Дубай"},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
 ]
 
 
@@ -27,20 +27,51 @@ def get_hotels(
 #body, request body
 @app.post("/hotels")
 def create_hotel(
-        title : str = Body(embed=True)
+        title : str = Body(embed=True)                                                              #embed делает json
 ):
     global hotels
     hotels.append({
         "id": hotels[-1]["id"] + 1,
         "title": title,
     })
+    return {"status" : "OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def put_hotel(
+        hotel_id: int,
+        title : str,
+        name : str
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotel["title"] = title
+            hotel["name"] = name
+    return {"status" : "OK"}
+
+
+@app.patch("/hotels/{hotel_id}")
+def patch_hotel(
+        hotel_id: int,
+        title : str | None = Query(None),
+        name : str | None = Query(None)
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            if title:
+                hotel["title"] = title
+            if name:
+                hotel["name"] = name
+    return {"status" : "OK"}
 
 
 @app.delete("/hotels/{hotel_id}")
 def delete_hotel(hotel_id: int):
     global hotels
     for hotel in hotels:
-        hotels = [hotel for hotel in hotels if hotel["id"]!= hotel_id]
+        hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
     return {"status": "OK"}
 
 
