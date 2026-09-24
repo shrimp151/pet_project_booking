ad7f71905bf6c39118151c83dfbe54908bade4ff
Никита Лобосов <lobosovnikita0@gmail.com>
2026-09-24 21:08:05 +0300

    create async_session_maker

--- src/database.py
+++ src/database.py
@@ -4,3 +4,7 @@ from src.config import settings
 
 
 engine = create_async_engine(settings.DB_URL)
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+session = async_session_maker()
